9f7d9980c979b41013fc0b913e2d9df96bed49f2
Oumaima El youssefi <oel-yous@e2r2p4.1337.ma>
2022-06-04 17:00:59 +0100

    friends, achievements and games added

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-
+<!-- Bronze, Silver, Gold, and Platinum -->
 <nav class="border-gray-200 px-2 sm:px-4 py-2.5 rounded  bg-neutral-700 ">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
 <!-- logo -->
@@ -28,6 +28,7 @@
         <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
     </div>
+    
 <!--  -->
 <!-- home chat game user -->
     <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1 relative" id="mobile-menu-3">
@@ -134,3 +135,4 @@ export default defineComponent({
   },
 });
 </script>
+
