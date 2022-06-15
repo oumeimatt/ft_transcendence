9f9af6a1457bc5b12a1096aaecf910e4c4c561f8
Oumaima El youssefi <oel-yous@e1r2p2.1337.ma>
2022-06-15 10:44:23 +0100

    height added in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      height: {
+        'screen-4' : '40vh'
+      }
+    }
   },
   plugins: [],
   variants: {
