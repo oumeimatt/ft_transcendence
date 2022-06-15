dffb938ae467ed4ce63d533663348f74c7c4be4e
oumeimatt <viet@Viets-MacBook-Pro.local>
2022-06-15 17:33:00 +0100

    updates

--- src/components/chatnavbar.vue
+++ src/components/chatnavbar.vue
@@ -1,111 +1,137 @@
 <template>
-    <div class="bg-neutral-500 border-t border-neutral-900 grid grid-flow-row auto-rows-max gap-px ">
+    <div class="bg-slate-800 border-t border-neutral-900 grid grid-flow-row auto-rows-max gap-px ">
         <div class="border-b w-4/5 border-black ml-8 pb-8">
           <button class="mt-12 flex justify-start items-center space-x-2" >
-            <svg @click="createRoom" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
+            <svg @click="createRoom" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 fill-slate-400" viewBox="0 0 20 20">
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
             </svg> 
-             <div @click="createRoom" class=" text-xl font-bold hover:underline "> Create new Room </div> 
+             <div @click="createRoom" class="text-slate-400 text-xl font-bold hover:underline "> Create new Room </div> 
           </button>
           <button class="mt-4 flex justify-start items-center space-x-2" >
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 fill-slate-400" fill="none" viewBox="0 0 24 24"  stroke-width="1">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
             </svg>
-            <span class="text-xl font-bold hover:underline"> All rooms </span> 
+            <span class="text-slate-400 text-xl font-bold hover:underline"> All rooms </span> 
           </button>
         </div>
         <div class="ml-8 mt-4 h-screen-4 border-b w-4/5 border-black  pb-8 ">
-          <h1 class="font-semibold text-xl mb-6 ">Rooms</h1>
+          <h1 class="text-slate-300 font-semibold text-xl mb-6 ">Rooms</h1>
           <div class="h-5/6 scrollbar scrollbar-track-zinc-900 scrollbar-thumb-zinc-600 ">
 
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/Public.png" class="ml-8 h-8 w-8"> <span class="font-semibold text-neutral-900 ">Public room 1 </span> 
-              </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 1 </span>
-              </div>
-              <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/Public.png" class="ml-8 h-8 w-8"> <span class="font-semibold text-neutral-900">Public room 2 </span>
-              </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 2 </span>
-              </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 3 </span>
-              </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 1 </span>
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/Public.png" class="ml-8 h-8 w-8"> <span class="font-semibold text-neutral-900">Public room 2 </span>
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="black" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/Public.png" class="ml-8 h-8 w-8"> <span class="font-semibold text-neutral-900">Public room 2 </span>
+                <img src="../assets/public.png" class=" ml-7 h-10 w-10 fill-slate-300" fill="none" viewBox="0 0 24 24">
+                <span class="font-semibold text-slate-400 hover:underline cursor-pointer">Public room 2 </span>
               </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 1 </span>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
               </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 1 </span>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
               </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 1 </span>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
               </div>
-              <div class="flex justify-start items-center space-x-4 mt-2"> 
-                <img src="../assets/Private.png" class="ml-8 h-6 w-6"> <span class="font-semibold text-neutral-900">Private room 1 </span>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <img src="../assets/public.png" class=" ml-7 h-10 w-10 fill-slate-300" fill="none" viewBox="0 0 24 24">
+                <span class="font-semibold text-slate-400 hover:underline cursor-pointer">Public room 2 </span>
+              </div>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <img src="../assets/public.png" class=" ml-7 h-10 w-10 fill-slate-300" fill="none" viewBox="0 0 24 24">
+                <span class="font-semibold text-slate-400 hover:underline cursor-pointer">Public room 2 </span>
+              </div>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
+              </div>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <img src="../assets/public.png" class=" ml-7 h-10 w-10 fill-slate-300" fill="none" viewBox="0 0 24 24">
+                <span class="font-semibold text-slate-400 hover:underline cursor-pointer">Public room 2 </span>
+              </div>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <svg xmlns="http://www.w3.org/2000/svg" class=" ml-8 h-8 w-8 fill-slate-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+                 <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">Private room 1 </span> 
+              </div>
+              <div class="flex justify-start items-center space-x-2 mt-2"> 
+                <img src="../assets/public.png" class=" ml-7 h-10 w-10 fill-slate-300" fill="none" viewBox="0 0 24 24">
+                <span class="font-semibold text-slate-400 hover:underline cursor-pointer">Public room 2 </span>
               </div>
           </div>
         </div>
         <div class="ml-8 h-screen-4 mt-4 w-4/5 border-black  pb-8  ">
-          <h1 class="font-semibold text-xl mb-6">Friends</h1>
+          <h1 class="text-slate-300 font-semibold text-xl mb-6">Friends</h1>
           <div class=" h-5/6 scrollbar scrollbar-track-zinc-900 scrollbar-thumb-zinc-600">
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/oel-yous.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">oumixa </span> 
+                <img src="../assets/oel-yous.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">oumixa </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/oel-yous.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">oumixa </span> 
+                <img src="../assets/oel-yous.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">oumixa </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/iidzim.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">ikram </span> 
+                <img src="../assets/iidzim.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">ikram </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/framdani.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">fatima zahra </span> 
+                <img src="../assets/framdani.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">fatima zahra </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
               <div class="flex justify-start items-center space-x-2 mt-2"> 
-                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-neutral-900 ">mlachheb </span> 
+                <img src="../assets/mlachheb.jpeg" class="ml-8 h-8 w-8 rounded-full"> <span class="font-semibold text-slate-400 hover:underline cursor-pointer ">mlachheb </span> 
               </div>
 
 
--- src/views/User.vue
+++ src/views/User.vue
@@ -9,7 +9,7 @@
       </div>
       <div v-if="usersInfo(username)" class="ml-20 font-semibold text-3xl top-12 absolute top-[440px] text-gray-400 "> {{ usersInfo(username).name }} </div>
       <div class='flex   rounded top-[322px] lg:top-58 absolute left-32 md:left-56'>
-          <button id='button' class=' mr-10 justify-center focus:outline-none space-between bg-neutral-500 hover:bg-neutral-800 font-small py-2 px-4 rounded inline-flex items-center'>
+          <button id='button' class=' mr-10 justify-center focus:outline-none space-between  bg-slate-500 hover:bg-gray-800 font-small py-2 px-4 rounded inline-flex items-center'>
             <!-- // add friend -->
             <!-- <svg id='icon' class='w-4 h-4 mr-1' fill='#FFF' stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="white"><path d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>
             <span id='text' class='text-white text-sm '>Add friend</span> -->
@@ -18,13 +18,13 @@
             <span id='text' class='text-white text-sm '> Friends</span>
           </button>
         </div>
-      <div class="flex  my-0 mx-auto w-3/5 bg-neutral-500 h-6 mb-6 relative">
-        <div class="bg-neutral-800 h-6 " style="width: 25%"> </div>
+      <div class="flex  my-0 mx-auto w-3/5 bg-slate-500 h-6 mb-6 relative">
+        <div class="bg-slate-700 h-6 " style="width: 25%"> </div>
         <p class="inline-block z-10 absolute left-2/4 -translate-x-2/4 text-slate-400" > level 0 - 25%  </p>
       </div>
       <div class="cent p-8 ">
         <div class="grid md:grid-cols-1 lg:grid-cols-1 gap-1 lg-gap-1  text-center  ml-11 mr-11 mt-8" >
-          <div class="p-4 bg-neutral-500 rounded-md " > 
+          <div class="p-4  bg-slate-500 rounded-md " > 
             <p class="text-2xl font-semibold pb-4"> Friends </p>
             <div v-if="usersInfo(username)" class="pt-4 border-t border-neutral-800 flex items-scretch space-x-2">
               <div v-for="friend in usersInfo(username).friends" :key="friend">
@@ -34,26 +34,26 @@
           </div>
         </div>
         <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-2 lg-gap-2 text-center ml-11 mr-11 mt-8" >
-          <div class="p-4 bg-neutral-500 rounded-md " > 
+          <div class="p-4  bg-slate-500 rounded-md " > 
             <div v-if="usersInfo(username)"> 
               <p  class="text-2xl font-semibold pb-4 border-b border-neutral-800"> Achievements </p>
               <!-- acheivement -->
               <div v-for="achievement in usersInfo(username).achievements" :key="achievement">
-                <div v-if="achievement == 'first'" class="grid grid-cols-8 justify-items-start bg-neutral-500 pt-4">
+                <div v-if="achievement == 'first'" class="grid grid-cols-8 justify-items-start  bg-slate-500 pt-4">
                     <div class="place-self-start" > <img src="../assets/medal.png" class="w-10 h-10"></div>
-                    <div class="col-span-7"> <span class="text-xl  font-semibold text-neutral-700"> </span>     <span class="text-s font-semibold text-neutral-800" > Congratulations ! You won your first Game! </span> </div>
+                    <div class="col-span-7"> <span class="text-xl  font-semibold text-slate-900"> </span>     <span class="text-s font-semibold text-neutral-900" > Congratulations ! You won your first Game! </span> </div>
                 </div>
-                <div v-if="achievement == 'bronze'" class="grid grid-cols-8 justify-items-start bg-neutral-500 pt-4">
+                <div v-if="achievement == 'bronze'" class="grid grid-cols-8 justify-items-start  bg-slate-500 pt-4">
                     <div class="place-self-start" > <img src="../assets/bronze.png" class="w-10 h-10"></div>
-                    <div class="col-span-7"> <span class="text-xl  font-semibold text-neutral-700"> Bronze medal </span>     <span class="text-s font-semibold text-neutral-800" > Congratulations ! You won 5 GAMES! </span> </div>
+                    <div class="col-span-7"> <span class="text-xl  font-semibold text-slate-900"> Bronze medal </span>     <span class="text-s font-semibold text-neutral-900" > Congratulations ! You won 5 GAMES! </span> </div>
                 </div>
-                <div v-if="achievement == 'silver'" class="grid grid-cols-8 justify-items-start bg-neutral-500 pt-4">
+                <div v-if="achievement == 'silver'" class="grid grid-cols-8 justify-items-start  bg-slate-500 pt-4">
                     <div class="place-self-start" > <img src="../assets/silver.png" class="w-10 h-10"></div>
-                    <div class="col-span-7"> <span class="text-xl  font-semibold text-neutral-700"> Silver medal </span>   <span class="text-s font-semibold text-neutral-800" >  Congratulations ! You won 10 GAMES! </span> </div>
+                    <div class="col-span-7"> <span class="text-xl  font-semibold text-slate-900"> Silver medal </span>   <span class="text-s font-semibold text-neutral-900" >  Congratulations ! You won 10 GAMES! </span> </div>
                 </div>
-                <div v-if="achievement == 'gold'" class="grid grid-cols-8 justify-items-start bg-neutral-500 pt-4">
+                <div v-if="achievement == 'gold'" class="grid grid-cols-8 justify-items-start  bg-slate-500 pt-4">
                     <div class="place-self-start" > <img src="../assets/gold.png" class="w-10 h-10"></div>
-                    <div class="col-span-7"> <span class="text-xl  font-semibold text-neutral-700"> Gold medal </span >  <span class="text-s font-semibold text-neutral-800" > Congratulations ! You won 20 GAMES! </span> </div>
+                    <div class="col-span-7"> <span class="text-xl  font-semibold text-slate-900"> Gold medal </span >  <span class="text-s font-semibold text-neutral-900" > Congratulations ! You won 20 GAMES! </span> </div>
                 </div>
               
               
@@ -62,34 +62,34 @@
               <!--  -->
             </div>
           </div>
-          <div class="p-4 bg-neutral-500 rounded-md " > 
+          <div class="p-4  bg-slate-500 rounded-md " > 
             <div > 
               <p class="text-2xl font-semibold pb-4 border-b border-neutral-800  "> Games </p>
               <!-- games  -->
               <div class="grid grid-cols-3 justify-itmes-center pt-4">
-                <div class="text-neutral-800 font-semibold "> oel-yous</div>
-                <div class="text-neutral-900 font-black "> 5 - 2  </div>
-                <div class="text-neutral-800 font-semibold "> soepkdsds  </div>
+                <div class="text-neutral-900 font-semibold "> oel-yous</div>
+                <div class="text-gray-900 font-black "> 5 - 2  </div>
+                <div class="text-neutral-900 font-semibold "> soepkdsds  </div>
               </div>
               <div class="grid grid-cols-3 justify-itmes-center pt-4">
-                <div class="text-neutral-800 font-semibold "> oel-yous</div>
-                <div class="text-neutral-900 font-black "> 5 - 2  </div>
-                <div class="text-neutral-800 font-semibold "> iidzim  </div>
+                <div class="text-neutral-900 font-semibold "> oel-yous</div>
+                <div class="text-gray-900 font-black "> 5 - 2  </div>
+                <div class="text-neutral-900 font-semibold "> iidzim  </div>
               </div>
               <div class="grid grid-cols-3 justify-itmes-center pt-4">
-                <div class="text-neutral-800 font-semibold "> oel-yous</div>
-                <div class="text-neutral-900 font-black "> 5 - 2  </div>
-                <div class="text-neutral-800 font-semibold "> framdani  </div>
+                <div class="text-neutral-900 font-semibold "> oel-yous</div>
+                <div class="text-gray-900 font-black "> 5 - 2  </div>
+                <div class="text-neutral-900 font-semibold "> framdani  </div>
               </div>
               <div class="grid grid-cols-3 justify-itmes-center pt-4">
-                <div class="text-neutral-800 font-semibold "> oel-yous</div>
-                <div class="text-neutral-900 font-black "> 5 - 2  </div>
-                <div class="text-neutral-800 font-semibold "> dkkdkksm  </div>
+                <div class="text-neutral-900 font-semibold "> oel-yous</div>
+                <div class="text-gray-900 font-black "> 5 - 2  </div>
+                <div class="text-neutral-900 font-semibold "> dkkdkksm  </div>
               </div>
               <div class="grid grid-cols-3 justify-itmes-center pt-4">
-                <div class="text-neutral-800 font-semibold "> oel-yous</div>
-                <div class="text-neutral-900 font-black "> 5 - 2  </div>
-                <div class="text-neutral-800 font-semibold "> ndjbjdjds  </div>
+                <div class="text-neutral-900 font-semibold "> oel-yous</div>
+                <div class="text-gray-900 font-black "> 5 - 2  </div>
+                <div class="text-neutral-900 font-semibold "> ndjbjdjds  </div>
               </div>
               <!--  -->
             </div>
