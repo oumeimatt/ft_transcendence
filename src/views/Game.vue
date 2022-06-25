3cc4c1453fe8cb5eb7e412f5643c557f3adfac5e
oumeimatt <viet@Viets-MacBook-Pro.local>
2022-06-25 09:59:21 +0100

    game

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
     <Header /> 
-    <h1 class="text-gray-400"> game page</h1>
+    <div class="mt-24 text-center text-gray-200 text-2xl font-bold mb-2">
+      <h1> Select difficulty</h1>
+    </div>
+    <div class="grid grid-rows-3 gap-y-2 text-center ">
+      <div class="mt-4"> 
+        <button class="bg-slate-300 w-4/12 h-small rounded text-xl font-bold hover:bg-slate-700 hover:ring-white ring-2  hover:text-gray-100">
+          Easy
+        </button>
+      </div>
+      <div class="mt-4"> 
+        <button class="bg-slate-400 w-4/12 h-small rounded text-xl font-bold hover:bg-slate-700 hover:ring-white ring-2  hover:text-gray-100">
+          Medium
+        </button>
+      </div>
+      <div class="mt-4"> 
+        <button class="bg-slate-500 w-4/12 h-small rounded text-xl font-bold hover:bg-slate-700 hover:ring-white ring-2  hover:text-gray-100">
+          Hard
+        </button>
+      </div>
+    </div>
   </div>
-  <footer />
+  <Footer />
 </template>
 
 <script lang="ts" setup>
