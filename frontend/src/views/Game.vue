e86ff4a52e03fc6ef3896cf7ee3733226fbdb5f1
mohamedamine456 <aminelachheb456@gmail.com>
2022-07-07 06:33:43 +0100

    push to branch

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -8,17 +8,12 @@
       <div class="grid grid-rows-3 gap-y-2 text-center ">
         <div class="mt-4"> 
           <button class="bg-slate-300 w-4/12 h-small rounded text-xl font-bold hover:bg-slate-700 hover:ring-white ring-2  hover:text-gray-100">
-            Easy
+            Default
           </button>
         </div>
         <div class="mt-4"> 
           <button class="bg-slate-400 w-4/12 h-small rounded text-xl font-bold hover:bg-slate-700 hover:ring-white ring-2  hover:text-gray-100">
-            Medium
-          </button>
-        </div>
-        <div class="mt-4"> 
-          <button class="bg-slate-500 w-4/12 h-small rounded text-xl font-bold hover:bg-slate-700 hover:ring-white ring-2  hover:text-gray-100">
-            Hard
+            Difficult
           </button>
         </div>
       </div>
@@ -33,6 +28,7 @@ import { inject } from 'vue';
 const store = inject('store')
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
+
 
 
 </script>
